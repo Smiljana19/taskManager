47cec7b92854ca2771562b47edece2944cebd62c
Smiljana Mihajlovic <smiljanamihajlovics19@gmail.com>
2021-07-23 16:27:43 +0200

    reset password page
    css,html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../app/login/login.component';
-import { SiginupComponent } from '../app/siginup/siginup.component'
+import { SiginupComponent } from '../app/siginup/siginup.component';
+import { ResetPasswordComponent } from '../app/reset-password/reset-password.component'
 
 const routes: Routes = [
-  {path: 'login', component:LoginComponent},
-  {path: '', component:SiginupComponent}
+  {path: '', component:LoginComponent},
+  {path: 'siginup', component:SiginupComponent},
+  {path: 'resetpassword', component:ResetPasswordComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SiginupComponent } from './siginup/siginup.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SiginupComponent
+    SiginupComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
